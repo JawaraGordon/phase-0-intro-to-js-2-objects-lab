039a8272365470eaf912a507781cd68e60a241a6
JawaraGordon <jawaragordon@icloud.com>
2022-03-12 14:35:34 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,60 @@
 // Write your solution in this file!
+const employee = {};
+
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    return {
+        ...obj,
+        [key]: value,
+      };
+  };
+
+ 
+  updateEmployeeWithKeyAndValue(employee,name,Sam);
+
+//   beforeEach(function () {
+//     for (const key in employee) {
+//       delete employee[key];
+//     }
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value){
+    obj[key] = value
+    return obj
+    };
+
+    destructivelyUpdateEmployeeWithKeyAndValue(employee,name,Sam);
+
+    //   beforeEach(function () {
+    // for (const key in employee) {
+    //   delete employee[key];
+    // }
+
+function deleteFromEmployeeByKey(obj, key, value){
+    const newObj = { ...obj };
+
+    newObj[key] = value;
+  
+    return newObj
+    
+};
+
+deleteFromEmployeeByKey(employee, name, Sam);
+
+function destructivelyDeleteFromEmployeeByKey(obj, key, value){
+    delete obj[key];
+    return obj
+  };
+
+  destructivelyDeleteFromEmployeeByKey(employee, key, value)
+
+// function updateEmployeeWithKeyAndValue(employee, key, value){
+    
+//     const newObj = { ...obj };
+
+//     newObj[key] = value;
+
+//     return newObj;
+// };
+
+// updateEmployeeWithKeyAndValue(employee,name,Sam,);
+
